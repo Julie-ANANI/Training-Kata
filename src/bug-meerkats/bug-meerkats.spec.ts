cb3b69eb6275e96b18ed8df8ee7ed4b484a2dab6
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2024-03-18 21:27:02 +0100

    refacto test for specific value of idx and not array

--- src/bug-meerkats/bug-meerkats.spec.ts
+++ src/bug-meerkats/bug-meerkats.spec.ts
@@ -5,25 +5,49 @@ describe('bug-meerkats', () => {
     test('should return head in first', () => {
         // Act
         const input = ['tail', 'body', 'head' ];
-        const expected = ['head', 'tail', 'body'];
+        const expected = 'head';
 
         // Arrange
         const result = fixTheMeerkat(input);
 
         // Assert
-        expect(result).toStrictEqual(expected)
+        expect(result[0]).toBe(expected);
     });
 
-    test('should return head in first idx is el equal to head', () => {
+    test('should return head always first idx is el equal to head', () => {
         // Act
         const input = ['body', 'head', 'tail'];
-        const expected = ['head', 'body', 'tail'];
+        const expected = 'body';
 
         // Arrange
         const result = fixTheMeerkat(input);
 
         // Assert
-        expect(result).toStrictEqual(expected)
+        expect(result[1]).toBe(expected);
+    });
+
+    test('should return body always second idx is el equal to body', () => {
+        // Act
+        const input = ['body', 'head', 'tail'];
+        const expected =  'tail';
+
+        // Arrange
+        const result = fixTheMeerkat(input);
+
+        // Assert
+        expect(result[2]).toBe(expected);
+    });
+
+    test('should return tail always second idx is el equal to tail', () => {
+        // Act
+        const input = ['body', 'tail', 'head'];
+        const expected = 'tail';
+
+        // Arrange
+        const result = fixTheMeerkat(input);
+
+        // Assert
+        expect(result[2]).toBe(expected);
     });
 
 });
--- src/bug-meerkats/bug-meerkats.ts
+++ src/bug-meerkats/bug-meerkats.ts
@@ -1,11 +1,28 @@
 export function fixTheMeerkat(arr: string[]) {
+  const first = 'head';
+  const second = 'body';
+  const third = 'tail';
   const result = arr;
   arr.forEach(( el, idx) => {
-    if(el === 'head'){
+    if(el === first){
       const elMove = result.splice(idx, 1)[0];
       result.unshift(elMove);
       return result;
     }
+
+    if(el === second){
+      const elMove = result.splice(idx, 1)[0];
+      //result.unshift(elMove);
+      result.splice(1, 0, elMove);
+      return result;
+    }
+
+    if(el === third){
+      const elMove = result.splice(idx, 1)[0];
+      result.push(elMove);
+      return result;
+    }
+
   });
   arr = result;
   return arr;
